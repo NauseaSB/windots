70ab22efde14d21e2f9884e7bfec7673ac0dd88f
NauseaSB <andrianfebriansayh@gmail.com>
2026-07-23 15:43:08 +0700

    added nvim config related folder and files

---
+++ .config/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "xiyaowong/transparent.nvim",
+    lazy = false,
+    priority = 1500,
+    opts = {
+      extra_groups = {
+        "NormalFloat",
+        "NvimTreeNormal",
+        "NvimTreeNormalNC",
+        "NeoTreeNormal",
+        "NeoTreeNormalNC",
+      },
+      exclude_groups = {},
+    },
+  },
+}
